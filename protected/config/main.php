13aac8999ac2c9f36237a00eff131f06343c1710
Alexander <Alexander@192.168.100.54>
2013-02-25 10:12:16 +0100

    added extensions physically and in config

--- protected/config/main.php
+++ protected/config/main.php
@@ -9,29 +9,40 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Web Application',
 
-	// preloading 'log' component
-	'preload'=>array('log'),
+	// preloading components
+	'preload'=>array('bootstrap','log', 'fancybox'),
 
 	// autoloading model and component classes
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
+                'application.extensions.*'
 	),
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'password',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
-		),
-		*/
+			'generatorPaths' => array(
+          '		bootstrap.gii'
+			),
+                    ),
 	),
 
 	// application components
 	'components'=>array(
+		'bootstrap' => array(
+	    'class' => 'ext.bootstrap.components.Bootstrap',
+	    'responsiveCss' => true,
+		),
+            
+                'fancybox' => array(
+                    'class' => 'ext.fancybox.EFancyBox'
+                ),
 		'user'=>array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
