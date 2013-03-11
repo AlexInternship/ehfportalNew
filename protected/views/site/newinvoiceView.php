70a04f8df7defa4d3d80c6b78259569be84bbbd9
Jes <jrkristensen@gmail.com>
2013-03-11 10:07:57 +0100

    added all relavent fields for the form

--- protected/views/site/newinvoiceView.php
+++ protected/views/site/newinvoiceView.php
@@ -17,15 +17,34 @@
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
 	<?php echo $form->errorSummary(array($Users, $Address, $Ourinvoices)); ?>
+        <div class="box"> 
         <div class="row">Afsender</div>
         <div class="row">
-		<?php echo $form->labelEx($Address,'cvr', array('class'=>'test')); ?>
+		<?php echo $form->labelEx($Address,'Organisations Nummmer', array('class'=>'test')); ?>
 		<?php echo $form->textField($Address,'cvr'); ?>
 		<?php echo $form->error($Address,'cvr'); ?>
 	</div>
-        
-        
-        
+        <div class="row">
+		<?php echo $form->labelEx($Users,'Firma Navn', array('class'=>'test')); ?>
+		<?php echo $form->textField($Users,'email'); ?>
+		<?php echo $form->error($Users,'email'); ?>
+	</div>
+        <div class="row">
+		<?php echo $form->labelEx($Users,'Adresse', array('class'=>'test')); ?>
+		<?php echo $form->textField($Users,'email'); ?>
+		<?php echo $form->error($Users,'email'); ?>
+	</div>
+        <div class="row">
+		<?php echo $form->labelEx($Users,'Postnummer', array('class'=>'test')); ?>
+		<?php echo $form->textField($Users,'email'); ?>
+		<?php echo $form->error($Users,'email'); ?>
+	</div>
+        <div class="row">
+		<?php echo $form->labelEx($Users,'By', array('class'=>'test')); ?>
+		<?php echo $form->textField($Users,'email'); ?>
+		<?php echo $form->error($Users,'email'); ?>
+	</div>
+ 
         <div class="row">
 		<?php echo $form->labelEx($Users,'email', array('class'=>'test')); ?>
 		<?php echo $form->textField($Users,'email'); ?>
@@ -40,35 +59,185 @@
 		<?php echo $form->labelEx($Ourinvoices,'invoicenumber', array('class'=>'test')); ?>
 		<?php echo $form->textField($Ourinvoices,'invoicenumber'); ?>
 		<?php echo $form->error($Ourinvoices,'invoicenumber'); ?>
-	</div>
+        </div>
+</div>
+        <div class="box">
         <div class="row">Modtager</div>
+        <div class="row">
+		<?php echo $form->labelEx($Ourinvoices,'Organisations nummer', array('class'=>'test')); ?>
+		<?php echo $form->textField($Ourinvoices,'invoicenumber'); ?>
+		<?php echo $form->error($Ourinvoices,'invoicenumber'); ?>
+	</div>
+        <div class="row">
+		<?php echo $form->labelEx($Ourinvoices,'Firma navn', array('class'=>'test')); ?>
+		<?php echo $form->textField($Ourinvoices,'invoicenumber'); ?>
+		<?php echo $form->error($Ourinvoices,'invoicenumber'); ?>
+	</div>
+        <div class="row">
+		<?php echo $form->labelEx($Ourinvoices,'Adresse', array('class'=>'test')); ?>
+		<?php echo $form->textField($Ourinvoices,'invoicenumber'); ?>
+		<?php echo $form->error($Ourinvoices,'invoicenumber'); ?>
+	</div>
+         <div class="row">
+		<?php echo $form->labelEx($Users,'Postnummer', array('class'=>'test')); ?>
+		<?php echo $form->textField($Users,'email'); ?>
+		<?php echo $form->error($Users,'email'); ?>
+	</div>
+        <div class="row">
+		<?php echo $form->labelEx($Users,'By', array('class'=>'test')); ?>
+		<?php echo $form->textField($Users,'email'); ?>
+		<?php echo $form->error($Users,'email'); ?>
+	</div>
+        <div class="row">
+		<?php echo $form->labelEx($Users,'Kontakt id', array('class'=>'test')); ?>
+		<?php echo $form->textField($Users,'email'); ?>
+		<?php echo $form->error($Users,'email'); ?>
+	</div>
+        <div class="row">
+		<?php echo $form->labelEx($Users,'email', array('class'=>'test')); ?>
+		<?php echo $form->textField($Users,'email'); ?>
+		<?php echo $form->error($Users,'email'); ?>
+	</div>
         
-        
-        
+</div>
+        <div class="box">       
         <div class="row">Leverings Adresse</div>
         <div class="row">
-		<?php echo $form->labelEx($Address,'address1', array('class'=>'test')); ?>
+		<?php echo $form->labelEx($Address,'Modtager', array('class'=>'test')); ?>
 		<?php echo $form->textField($Address,'address1'); ?>
 		<?php echo $form->error($Address,'address1'); ?>
 	</div>
-        
         <div class="row">
-		<?php echo $form->labelEx($Address,'city', array('class'=>'test')); ?>
+		<?php echo $form->labelEx($Address,'address', array('class'=>'test')); ?>
+		<?php echo $form->textField($Address,'address1'); ?>
+		<?php echo $form->error($Address,'address1'); ?>
+	</div>
+        <div class="row">
+		<?php echo $form->labelEx($Address,'By', array('class'=>'test')); ?>
 		<?php echo $form->textField($Address,'city'); ?>
 		<?php echo $form->error($Address,'city'); ?>
 	</div>
-
 	<div class="row">
-		<?php echo $form->labelEx($Address,'zip', array('class'=>'test')); ?>
+		<?php echo $form->labelEx($Address,'Postnummer', array('class'=>'test')); ?>
 		<?php echo $form->textField($Address,'zip'); ?>
 		<?php echo $form->error($Address,'zip'); ?>
 	</div>
-        
-        
+</div>
+        <div class="box">
         <div class="row">Faktura dato og referancer</div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Type', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Faktura nummer', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Faktura dato', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Leverings dato', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Ordre Reference', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Person Reference', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Konto nummer', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Noter', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Leverings dato', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Valuta', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        </div>
+        <div class="box">
         <div class="row">Vare linier</div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Vare nummer', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Beskrivelse', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Antal', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Pris', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Moms', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Rabat', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Beløb', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        </div>
+        <div class="box">
         <div class="row">Betaling</div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Betalings dato', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Betalings form', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">
+        <?php echo $form->labelEx($Address,'Rabat', array('class'=>'test')); ?>
+        <?php echo $form->textField($Address,'zip'); ?>
+	<?php echo $form->error($Address,'zip'); ?>
+        </div>
+        <div class="row">TODO: Lav funtion der ændre input felter der passer til betalings form</div>
+        
+        </div>
+        <div class="box">
         <div class="row">Bilag</div>
+        <div class="row">TODO: fil uploader </div>
+        </div>
         
 <div class="row buttons">
 <?php echo CHtml::submitButton($Address->isNewRecord ? 'Send faktura' : 'Save'); ?>
