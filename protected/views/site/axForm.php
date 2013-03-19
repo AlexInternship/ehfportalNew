49e67fbf3462dcbc4c8b57488dc3c7df8f940227
Jes <jrkristensen@gmail.com>
2013-03-19 12:57:19 +0100

    Axform lavet
    mangler action i sitecontroller

---
+++ protected/views/site/axForm.php
@@ -0,0 +1,49 @@
+<?php ?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'contact-form',
+	'enableClientValidation'=>true,
+	'clientOptions'=>array(
+		'validateOnSubmit'=>true,
+	),
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+        <div class="row">
+            <?php $form->labelEx($model, 'momsfri')?>
+            <?php $form->textField($model, 'momsfri')?>
+            <?php $form->error($model, 'momsfri')?> 
+        </div>
+        <div class="row">
+            <?php $form->labelEx($model, 'antal')?>
+            <?php $form->textField($model, 'antal')?>
+            <?php $form->error($model, 'antal')?> 
+        </div>
+        <div class="row">
+            <?php $form->labelEx($model, 'pris')?>
+            <?php $form->textField($model, 'pris')?>
+            <?php $form->error($model, 'pris')?> 
+        </div>
+        <div class="row">
+            <?php $form->labelEx($model, 'konterting')?>
+            <?php $form->textField($model, 'konterting')?>
+            <?php $form->error($model, 'konterting')?> 
+        </div>
+        <div class="row">
+            <?php $form->labelEx($model, 'dimaccount')?>
+            <?php $form->textField($model, 'dimaccount')?>
+            <?php $form->error($model, 'dimaccount')?> 
+        </div>
+        	<div class="row buttons">
+		<?php echo CHtml::submitButton('Submit'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div>
+        
