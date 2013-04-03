0809f95fc872353730df9a2358803717aa2c0a3f
Alexander Schmidt <childofstars@gmail.com>
2013-04-03 10:13:54 +0200

    new file:   protected/models/Document.php 	new file:   protected/models/SerializeDocument.php 	new file:   protected/models/SerializedDocuments.php

---
+++ protected/models/Document.php
@@ -0,0 +1,103 @@
+<?php
+
+class Document extends CFormModel{
+    
+      public $Users; 
+      public $username;   
+      public $firstname;   
+      public $lastname;   
+      public $email;  
+      public $Address; 
+      public $phone;   
+      public $address;   
+      public $address;   
+      public $address;   
+      public $zip;   
+      public $city;
+      public $address;
+      public $address;
+      public $address;
+      public $zip;
+      public $city;
+      public $Partners;
+      public $validcvr;
+      public $name;
+      public $name;
+      public $email;
+      public $validcvr;
+      public $Fakturadata;
+      public $varenavn;   
+      public $Varenummer;   
+      public $antal;   
+      public $Pris;   
+      public $Kontering;   
+      public $dimaccont;   
+      public $ehf_vat;  
+      public $Varebeloeb;
+      public $Skaffevare;
+      public $Ordrefradrag;
+      public $Momsfri;
+      public $order_id;
+      public $type;
+      public $momssats;
+      public $kortart;
+      public $giro;
+      public $girocreditor;
+      public $paymenttype;
+      public $bank;
+      public $account;
+      public $bic;
+      public $iban;
+      public $comment;
+      public $status;
+      public $behandler;
+      public $Seneste_rettidige_betalingsdato;
+      public $FakturaDato;
+      public $Leveringsdato;
+      public $Ordrenummer;
+      public $buyercontact;
+      public $sellercontact;
+      public $EANlokationsnr;
+      public $kontraktReference;
+      public $Fakturanummer;
+      public $Kreditnotanummer;
+      public $refno;   
+      public $orderContactName;
+      public $sellersOrderID;
+      public $Ordredato;
+      public $dimaccount;
+      public $KontantRabatSats;
+      public $KontantRabatDato;
+      public $StrafRenteDato;
+      public $shipping;
+      public $tax;
+      public $duty;
+      public $importchg;
+      public $Tillaeg;
+      public $Fradrag;
+      public $Varetotal;
+      public $Varebeloeb;
+      public $momsgrundlag;
+      public $linjetotal_eks_moms;
+      public $vat;
+      public $shipping;
+      public $tax;   
+      public $duty;   
+      public $importchg;   
+      public $Tillaeg;   
+      public $Fradrag;
+      public $ehf_momsgrundlag;
+      public $ehf_moms;
+      public $ehf_momsgrundlag_total;
+      public $Moms;   
+      public $yt;
+      
+      public function rules() {
+          
+      }
+      
+      public function validate() {
+          
+      }
+}
+?>
---
+++ protected/models/SerializeDocument.php
@@ -0,0 +1,29 @@
+<?php
+
+class SerializeDocument extends CFormModel{
+    
+   public $serializedocument_id;
+   public $type;
+   public $document;
+   public $id;
+   public $version;
+   public $outbox;
+
+   public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('document', 'required'),
+			array('id', 'numerical', 'integerOnly'=>true),
+			array('type', 'length', 'max'=>7),
+			array('version', 'length', 'max'=>10),
+			array('outbox', 'length', 'max'=>3),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('serializedocument_id, type, document, id, version, outbox', 'safe', 'on'=>'search'),
+		);
+	}
+    
+}
+?>
